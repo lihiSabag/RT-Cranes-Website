a72684c4a379f54123b8ae7cb28331af42976a81
lihisabag <lihisabag10@gmail.com>
2023-10-01 20:00:32 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // Set EJS as templating engine
 app.set("view engine", "ejs");
-
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -21,13 +20,13 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.render("pages/homePage");
-  
 });
 app.get("/homePage", (req, res) => {
-    res.render("pages/homePage");
-
-  });
-
+  res.render("pages/homePage");
+});
+app.get("/homePageEn", (req, res) => {
+  res.render("pages/homePageEn");
+});
 app.get("/aboutUs", (req, res) => {
   res.render("pages/aboutUs");
 });
@@ -55,48 +54,36 @@ app.get("/itemGRT8100", (req, res) => {
 app.get("/itemGRT8100En", (req, res) => {
   res.render("pages/itemGRT8100En");
 });
-app.get('/homePageEn', (req, res) => {
-  res.render("pages/homePageEn");
-});
-app.get('/En', (req, res) => {
-  res.render("pages/homePageEn");
-});
-
 
 // Endpoint to handle form submission
-app.post('/send-email', (req, res) => {
+app.post("/send-email", (req, res) => {
   const { fullName, mobile, email, message } = req.body;
   const myEmail = process.env.EMAIL;
   const password = process.env.PASSWORD;
 
   // Create a transporter using nodemailer
   const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-          user: myEmail, 
-          pass: password 
-      }
+    service: "gmail",
+    auth: {
+      user: myEmail,
+      pass: password,
+    },
   });
 
   const mailOptions = {
-      from: myEmail,
-      to: 'roaslihi@gmail.com', // Replace with your email
-      subject: 'New Contact Form Submission',
-      text: `Full Name: ${fullName}\nPhone: ${mobile}\nEmail: ${email}\n\n${message}`
+    from: myEmail,
+    to: "roaslihi@gmail.com", // Replace with your email
+    subject: "New Contact Form Submission",
+    text: `Full Name: ${fullName}\nPhone: ${mobile}\nEmail: ${email}\n\n${message}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          console.error('Error sending email:', error);
-          res.status(500).send('Error sending email');
-      } else {
-          console.log('Email sent:', info.response);
-          res.status(200).send('Email sent successfully');
-      }
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent:", info.response);
+      res.status(200).send("Email sent successfully");
+    }
   });
 });
-
-
-
-
-
